7f629d65b788ac2c432f3eac6d0db223b62e9aa5
Ki Cheuk Yung <kcyw23@gmail.com>
2026-01-11 04:09:59 +0100

    Initial project structure

---
+++ src/main/java/fr/eidd/projet_genie_logiciel/App.java
@@ -0,0 +1,13 @@
+package fr.eidd.projet_genie_logiciel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
